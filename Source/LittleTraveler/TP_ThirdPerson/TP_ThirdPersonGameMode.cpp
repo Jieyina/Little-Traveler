ac698882f6f8d3d8b8cbccb0b0470cefd71502e0
jieyi <nasjieyi@gmail.com>
2021-02-01 18:45:48 +0800

    change cpp test level

--- Source/LittleTraveler/TP_ThirdPerson/TP_ThirdPersonGameMode.cpp
+++ Source/LittleTraveler/TP_ThirdPerson/TP_ThirdPersonGameMode.cpp
@@ -7,7 +7,7 @@
 ATP_ThirdPersonGameMode::ATP_ThirdPersonGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/Test_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonBP/Blueprints/LittlePeople"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
